fb82814ac79edf050ce1ca6a9e5f1eb2b5972004
jasenmichael <jasen@jasenmichael.com>
2024-01-05 12:06:34 -0600

    test: Add dummy server for testing

---
+++ tests/dummy-server.cjs
@@ -0,0 +1,11 @@
+const http = require('http')
+
+const port = process.argv[2] // Port passed as argument from test
+
+const server = http.createServer((req, res) => {
+  res.end()
+})
+
+server.listen(port, () => {
+  console.log(`Dummy server listening on port ${port}`)
+})
